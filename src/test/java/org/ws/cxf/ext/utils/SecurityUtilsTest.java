4153043bb3be02c5d14c1154af16c944108d2197
idrissneumann <neumann.idriss@gmail.com>
2015-12-01 21:54:58 +0100

    Adding MD5 support and getting ip from client request

--- src/test/java/org/ws/cxf/ext/utils/SecurityUtilsTest.java
+++ src/test/java/org/ws/cxf/ext/utils/SecurityUtilsTest.java
@@ -1,8 +1,12 @@
 package org.ws.cxf.ext.utils;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Map;
+
+import org.apache.commons.collections.MapUtils;
 import org.junit.Test;
 import org.ws.cxf.ext.AbstractTest;
 
@@ -44,5 +48,51 @@ public class SecurityUtilsTest extends AbstractTest {
 		System.out.println("testGetSHA1HmacQuietlyFail hash2 = " + hash2);
 
 		assertFalse(SecurityUtils.isEquals(hash, hash2));
+	}
+
+	/**
+	 * Nominal test.
+	 */
+	@Test
+	public final void testGetMD5HashQuietlyNominal() {
+		String str = "test";
+		String hash = SecurityUtils.getMD5HashQuietly(str);
+		String hash2 = SecurityUtils.getMD5HashQuietly(str);
+
+		assertTrue(SecurityUtils.isEquals(hash, hash2));
+	}
+
+	/**
+	 * Fail test
+	 */
+	@Test
+	public final void testGetMD5HashQuietlyFail() {
+		String str = "test";
+		String str2 = "tesssst2";
+		String hash = SecurityUtils.getMD5HashQuietly(str);
+		String hash2 = SecurityUtils.getMD5HashQuietly(str2);
+
+		assertFalse(SecurityUtils.isEquals(hash, hash2));
+	}
+
+	/**
+	 * Test sur l'adresse IP.
+	 */
+	@Test
+	public final void testGetIPAdressQuietly() {
+		String ip = SecurityUtils.getIPAdressQuietly();
+		System.out.println("ip = " + ip);
+		assertNotNull(ip);
+	}
+
+	/**
+	 * Test sur les infos de localisation
+	 */
+	@Test
+	public final void testGetClientLocalisationInfos() {
+		Map<String, String> map = SecurityUtils.getClientLocalisationInfos("197.17.201.138");
+		System.out.println("infos = " + JSONUtils.map2json(map));
+		assertNotNull(map);
+		assertTrue(MapUtils.isNotEmpty(map));
 	}
 }
