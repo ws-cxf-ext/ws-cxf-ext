17a835bf560e8939ebd160643cdb6491dcbc160d
idrissneumann <neumann.idriss@gmail.com>
2015-12-28 22:52:15 +0100

    New exception handler

--- src/main/java/org/ws/cxf/ext/utils/others/ExceptionHandler.java
+++ src/main/java/org/ws/cxf/ext/utils/others/ExceptionHandler.java
@@ -1,22 +1,37 @@
 package org.ws.cxf.ext.utils.others;
 
+import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
-import org.ws.cxf.ext.exception.FonctionalException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Exception handler.
  * 
- * @author Christian Cougourdan
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
  */
-public class ExceptionHandler implements ExceptionMapper<FonctionalException> {
+public class ExceptionHandler implements ExceptionMapper<Exception> {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
+
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public Response toResponse(FonctionalException exception) {
-		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).header("Exception", exception.getMessage()).build();
+	public Response toResponse(Exception exception) {
+		Integer code = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+
+		if (exception instanceof ClientErrorException) {
+			code = ((ClientErrorException) exception).getResponse().getStatus();
+		}
+
+		LOGGER.error("Here is an exception", exception);
+
+		return Response.status(code) //
+		        .header("Exception", exception.getMessage()) //
+		        .header("ExceptionType", exception.getClass().getSimpleName()) //
+		        .build();
 	}
 
 }
--- src/main/java/org/ws/cxf/ext/utils/others/ResponseExceptionHandler.java
+++ src/main/java/org/ws/cxf/ext/utils/others/ResponseExceptionHandler.java
@@ -1,21 +1,36 @@
 package org.ws.cxf.ext.utils.others;
 
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.ResponseExceptionMapper;
 import org.ws.cxf.ext.exception.FonctionalException;
+import org.ws.cxf.ext.exception.TechnicalException;
 
 /**
  * Getting and process exception that is thrown by a RESTful webservice.
  * 
- * @author Christian Cougourdan
+ * @author Idriss Neumann <neumann.idriss@gmail.com>
  */
-public class ResponseExceptionHandler implements ResponseExceptionMapper<FonctionalException> {
+public class ResponseExceptionHandler implements ResponseExceptionMapper<Exception> {
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public FonctionalException fromResponse(Response r) {
-		return new FonctionalException(r.getHeaderString("Exception"));
+	public Exception fromResponse(Response r) {
+		String typeOfException = r.getHeaderString("ExceptionType");
+		String message = r.getHeaderString("Exception");
+
+		if (FonctionalException.class.getSimpleName().equalsIgnoreCase(typeOfException)) {
+			return new FonctionalException(message);
+		} else if (BadRequestException.class.getSimpleName().equalsIgnoreCase(typeOfException)) {
+			return new BadRequestException(message);
+		} else if (NotFoundException.class.getSimpleName().equalsIgnoreCase(typeOfException)) {
+			return new NotFoundException(message);
+		} else {
+			return new TechnicalException(message);
+		}
+
 	}
 }
